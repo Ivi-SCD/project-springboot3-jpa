74c108800ccabd115b1c82da32f7519edc5ff219
Ivi-SCHP <ivipnascimento@hotmail.com>
2022-10-13 10:55:12 -0300

    Service layer, component registration

--- src/main/java/com/iviprojects/course/repositories/UserRepository.java
+++ src/main/java/com/iviprojects/course/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.iviprojects.course.entities.User;
 
-public interface UserRepository extends JpaRepository<User, Long>{
+//Opcional a anottation do @Repository pois ele já herda de JPARepository [!]
+public interface UserRepository extends JpaRepository <User, Long> {
 	
 }
