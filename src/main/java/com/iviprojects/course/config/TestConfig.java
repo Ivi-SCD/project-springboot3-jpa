52079c342056cef3eeeb598ff07bde74c172ab7e
Ivi <ivipnascimento@hotmail.com>
2022-10-18 19:28:39 -0300

    OrderItem, many-to-many association with extra attributes Part 2

--- src/main/java/com/iviprojects/course/config/TestConfig.java
+++ src/main/java/com/iviprojects/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.iviprojects.course.entities.Category;
 import com.iviprojects.course.entities.Order;
+import com.iviprojects.course.entities.OrderItem;
 import com.iviprojects.course.entities.Product;
 import com.iviprojects.course.entities.User;
 import com.iviprojects.course.entities.enums.OrderStatus;
 import com.iviprojects.course.repositories.CategoryRepository;
+import com.iviprojects.course.repositories.OrderItemRepository;
 import com.iviprojects.course.repositories.OrderRepository;
 import com.iviprojects.course.repositories.ProductRepository;
 import com.iviprojects.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -58,6 +63,13 @@ public class TestConfig implements CommandLineRunner {
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice()); 
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice()); 
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice()); 
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
 		p1.getCategories().add(cat2);
 		p2.getCategories().add(cat1);
