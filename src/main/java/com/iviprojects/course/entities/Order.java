91ef91fabc591f8f451d0a67f43ee5690eb1b329
Ivi <ivipnascimento@hotmail.com>
2022-10-20 10:24:28 -0300

    Subtotal and total methods

--- src/main/java/com/iviprojects/course/entities/Order.java
+++ src/main/java/com/iviprojects/course/entities/Order.java
@@ -105,6 +105,14 @@ public class Order implements Serializable {
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
+	
+	public Double getTotal() {
+		double total = 0;
+		for (OrderItem orderItem: items) {
+			total += orderItem.getSubTotal();
+		}
+		return total;
+	}
 
 	@Override
 	public boolean equals(Object obj) {
