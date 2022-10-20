49991e6f40f257875c208843022c53611f9c4bfe
Ivi <ivipnascimento@hotmail.com>
2022-10-20 10:38:10 -0300

    User insert

--- src/main/java/com/iviprojects/course/resources/UserResource.java
+++ src/main/java/com/iviprojects/course/resources/UserResource.java
@@ -1,14 +1,18 @@
 package com.iviprojects.course.resources;
 
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.iviprojects.course.entities.User;
 import com.iviprojects.course.services.UserService;
@@ -21,7 +25,7 @@ public class UserResource {
 	private UserService userService;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity <List<User>> findAll(){
 		List <User> listUsers = userService.findAll();
 		
 		return ResponseEntity.ok().body(listUsers);
@@ -32,5 +36,15 @@ public class UserResource {
 		User user = userService.findById(id);
 		
 		return ResponseEntity.ok().body(user);
+	}
+	
+	@PostMapping
+	public ResponseEntity <User> insertUser(@RequestBody User user){
+		user = userService.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(user.getId()).toUri();
+	
+		
+		return ResponseEntity.created(uri).body(user);
 	}
 }
